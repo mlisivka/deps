037df5fc6f92a62df3ca5e4dbdaa5078120baa85
Mykola Lisivka <mykola.lisivka@gmail.com>
2020-10-23 21:49:57 +0300

    Initial commit. Add library that creates dependency graph

---
+++ lib/deps.rb
@@ -0,0 +1,91 @@
+require 'ruby-graphviz'
+
+# TODO: Filter by path
+# TODO: Filter by class with stack size
+# TODO: :with_methods, :class_only
+# TODO: Do not self-referrance
+# TODO: meta programming
+class Deps
+  attr_reader :stack_level
+  def initialize
+    @callees = []
+    @deps = []
+    @stack_level = 0
+  end
+
+  def add_callee(called_class, method)
+    @callees.prepend [called_class, method, stack_level]
+  end
+
+  def match_dependency
+    add_dep(find_caller, latest_called) if find_caller
+  end
+
+  def previous_stack_level
+    stack_level - 1
+  end
+
+  def find_caller
+    @callees.find do |called_pair|
+      called_pair.last == previous_stack_level
+    end
+  end
+
+  def latest_called
+    @callees.find do |called_pair|
+      called_pair.last == stack_level
+    end
+  end
+
+  def add_dep(caller_triple, called_triple)
+    @deps << [caller_triple, called_triple]
+  end
+
+  def dependency_graph
+    @deps.map do |a, b|
+      [a[0..1].join('#'), b[0..1].join('#')]
+    end.uniq
+  end
+
+  def change_stack_level_by(number)
+    @stack_level += number
+  end
+
+  def create_graph_image
+    return if dependency_graph.empty?
+
+    g = GraphViz.new('G', use: 'dot', mode: 'major', rankdir: 'LR', concentrate: 'true', fontname: 'Arial')
+    dependency_graph.each do |k, vs|
+      n1 = g.add_nodes(k)
+      n2 = g.add_nodes(vs)
+      g.add_edges(n1, n2)
+    end
+    g.output(png: 'deps.png')
+  end
+
+  def self.run
+    self.test_run { yield }.tap { |deps| deps.create_graph_image }
+  end
+
+  def self.test_run
+    deps = Deps.new
+    @trace1 = TracePoint.new(:call) do |tp|
+      class_name = tp.defined_class.to_s
+      method = tp.method_id.to_s
+      deps.change_stack_level_by(1)
+      # deps.increase_stack_level
+      deps.add_callee(class_name, method)
+    end
+    @trace2 = TracePoint.new(:return) do |tp|
+      deps.match_dependency
+      # deps.descrease_stack_level
+      deps.change_stack_level_by(-1)
+    end
+    @trace1.enable
+    @trace2.enable
+    yield
+    @trace1.disable
+    @trace2.disable
+    deps
+  end
+end
---
+++ spec/lib/deps_spec.rb
@@ -0,0 +1,148 @@
+require './lib/deps'
+
+RSpec.describe Deps do
+  describe '.run' do
+    let(:file_path) { File.expand_path('deps.png') }
+
+    context 'when dependency_graph present' do
+      class Person
+        def phone_number
+          PhoneNumber.new.phone
+        end
+      end
+      class PhoneNumber
+        def phone
+          '+3809876543210'
+        end
+      end
+
+      after do
+        File.delete(file_path)
+      end
+
+      it 'creates graph image' do
+        Deps.run do
+          Person.new.phone_number
+        end
+        expect(File.exist?(file_path)).to be_truthy
+      end
+    end
+
+    context 'when dependency_graph is empty' do
+      it 'does not create graph image' do
+        Deps.run do
+        end
+        expect(File.exist?(file_path)).to be_falsey
+      end
+    end
+  end
+  context 'when there is no dependency' do
+    class Person
+      def first_name
+        'Hello'
+      end
+    end
+
+    it do
+      deps = Deps.test_run do
+        person = Person.new
+        person.first_name
+      end
+      expect(deps.dependency_graph).to eq []
+    end
+  end
+
+  context 'with simple dependency' do
+    class Person
+      def first_name
+        'Hello'
+      end
+
+      def phone_number
+        PhoneNumber.new.phone
+      end
+    end
+    class PhoneNumber
+      def phone
+        '+3809876543210'
+      end
+    end
+
+    it do
+      deps = Deps.test_run do
+        person = Person.new
+        person.first_name
+        person.phone_number
+      end
+      expect(deps.dependency_graph).to eq [['Person#phone_number', 'PhoneNumber#phone']]
+    end
+  end
+
+  context 'with complex dependencies' do
+    class Person
+      def first_name
+        'John'
+      end
+
+      def last_name
+        'Snow'
+      end
+
+      def middle_name
+        PhoneNumber.new.get
+      end
+    end
+    module StringUtils
+      def validate_str(string)
+        string
+      end
+    end
+    module NumberUtils
+      include StringUtils
+      def validate(number)
+        validate_str(number)
+      end
+    end
+    class PhoneNumber
+      include NumberUtils
+
+      def get
+        validate_str(validate('+123'))
+      end
+    end
+    class Site
+      def initialize
+        @person = Person.new
+      end
+
+      def full_name
+        test
+        @person.first_name + @person.middle_name + @person.last_name
+      end
+
+      def print(text)
+        text
+      end
+
+      def test
+        print('Nothing')
+      end
+    end
+
+    it do
+      deps = Deps.test_run do
+        site = Site.new
+        site.full_name
+      end
+      expect(deps.dependency_graph).to eq [["Site#test", "Site#print"],
+                                           ["Site#full_name", "Site#test"],
+                                           ["Site#full_name", "Person#first_name"],
+                                           ["NumberUtils#validate", "StringUtils#validate_str"],
+                                           ["PhoneNumber#get", "NumberUtils#validate"],
+                                           ["PhoneNumber#get", "StringUtils#validate_str"],
+                                           ["Person#middle_name", "PhoneNumber#get"],
+                                           ["Site#full_name", "Person#middle_name"],
+                                           ["Site#full_name", "Person#last_name"]]
+    end
+  end
+end
